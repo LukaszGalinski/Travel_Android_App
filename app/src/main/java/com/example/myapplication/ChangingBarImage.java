3bcc976cebd3836e214a95639b52eac837fbb635
LukaszGalinski <Lipt0n12>
2019-12-19 18:33:17 +0100

    .gitignore repair

--- app/src/main/java/com/example/myapplication/ChangingBarImage.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication;
-
-import android.content.Context;
-import android.view.View;
-import android.widget.ImageButton;
-
-public interface ChangingBarImage {
-    void setImageBg(ImageButton relaks, ImageButton atraction, ImageButton hotel, ImageButton restaurants, ImageButton hospital, ImageButton university);
-}
